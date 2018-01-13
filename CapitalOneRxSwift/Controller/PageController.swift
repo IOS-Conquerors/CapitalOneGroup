72b95ba68c5c24986fbe8d80476d599b25d0f978
Justin <jbanes01@gmail.com>
2018-01-13 18:26:30 -0500

    Got 3 page views working, without images or data linked

---
+++ CapitalOneRxSwift/Controller/PageController.swift
@@ -0,0 +1,109 @@
+//
+//  ViewController.swift
+//  CapitalOneRxSwift
+//
+//  Created by Mac on 1/13/18.
+//  Copyright © 2018 Ghost World. All rights reserved.
+//
+
+import UIKit
+import RxSwift
+import RxCocoa
+
+//This will be for the initial page controller that shows you the 3 different people you can click
+class PageController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        dataSource = self
+        
+        
+        
+        
+        if let First = orderedViewControllers.first {
+            setViewControllers([First],
+                               direction: .forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    
+    private lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newViewController(name: "First"),
+                self.newViewController(name: "Second"),
+                self.newViewController(name: "Third")]
+    }()
+    
+    private func newViewController(name: String) -> UIViewController {
+        return UIStoryboard(name: "Main", bundle: nil) .
+            instantiateViewController(withIdentifier: "\(name)")
+    }
+
+    
+    
+    
+    
+    
+    func presentationCount(for pageViewController: UIPageViewController) -> Int {
+        return orderedViewControllers.count
+    }
+    
+    func presentationIndex(for pageViewController: UIPageViewController) -> Int {
+        guard let firstViewController = viewControllers?.first,
+            let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else {
+                return 0
+        }
+        
+        return firstViewControllerIndex
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+    
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ CapitalOneRxSwift/Model/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  CapitalOneRxSwift
+//
+//  Created by Mac on 1/13/18.
+//  Copyright © 2018 Ghost World. All rights reserved.
+//
+
+import Foundation
+
+
+class Constants{
+    static let lowNumber = "5555555555"
+    static let medNumber = "6666666666"
+    static let highNumber = "7777777777"
+}
